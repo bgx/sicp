0ec1956c83fa2b846b6eb98586bc852969450413
Brian Grady <bgx@users.noreply.github.com>
2017-10-26 20:20:05 -0400

    Add: Exercise 2.91
    Dividing univariate polynomials

--- chapter2.rkt
+++ chapter2.rkt
@@ -663,11 +663,17 @@
     (=zero?-poly-iter (term-list p)))
 
   (define (negate-poly p)
-    (make-poly (variable p)
-               (map
-                (lambda (term)
-                  (make-term (order term) (negate (coeff term))))
-                (term-list p))))
+    (make-poly (variable p) (negate-terms (term-list p))))
+
+  (define (div-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (map
+         (lambda (term-list)
+           (make-poly (variable p1) term-list))
+         (div-terms (term-list p1)
+                    (term-list p2)))
+        (error "Polys not in same var -- DIV-POLY"
+               (list p1 p2))))
 
   (define (add-terms L1 L2)
     (cond ((empty-termlist? L1) L2)
@@ -699,6 +705,29 @@
            (make-term (+ (order t1) (order t2))
                       (mul (coeff t1) (coeff t2)))
            (mul-term-by-all-terms t1 (rest-terms L))))))
+  (define (negate-term term)
+    (make-term (order term) (negate (coeff term))))
+  (define (negate-terms L)
+    (map negate-term L))
+  (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist) (the-empty-termlist))
+        (let ((t1 (first-term L1))
+              (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist) L1)
+              (let ((new-c (div (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((new-t (make-term new-o new-c)))
+                  (let ((rest-of-result
+                         (div-terms
+                          (add-terms
+                           L1
+                           (negate-terms
+                            (mul-term-by-all-terms new-t L2)))
+                          L2))))
+                  (list (adjoin-term new-t (car rest-of-result))
+                        (cadr rest-of-result))))))))
   
   ;; interface to rest of the system
   (define (tag p) (attach-tag 'polynomial p))
@@ -713,6 +742,8 @@
        (lambda (p) (tag (negate-poly p))))
   (put 'sub '(polynomial polynomial)
        (lambda (p1 p2) (tag (add-poly p1 (negate p2)))))
+  (put 'div '(polynomial polynomial)
+       (lambda (p1 p2) (map (lambda (p) (tag p)) (div-poly p1 p2))))
   
   'done)
 
