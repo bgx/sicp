00c133c17616f2d28ca6a22204101d1dc0a23691
Brian Grady <bgx@users.noreply.github.com>
2017-10-28 21:13:34 -0400

    Add: Exercise 3.2
    make-monitored -- returns a procedure that keeps track of the number of
    times it has been called

--- chapter3.rkt
+++ chapter3.rkt
@@ -12,5 +12,13 @@
     (set! sum (+ sum x))
     sum))
 
+(define (make-monitored f)
+  (let ((count 0))
+    (lambda (x)
+      (cond ((eq? x 'how-many-calls?) count)
+            ((eq? x 'reset-count) (set! count 0))
+            (else (set! count (+ count 1))
+                  (f x))))))
+
 ;;;;;;;;;;;;;; Experimental Below ;;;;;;;;;;;;;;;;;;
 
