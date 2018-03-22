acefcf82d8e0418384255315eafd5233e87688b1
Brian Grady <bgx@users.noreply.github.com>
2018-03-22 00:05:21 -0400

    Mod: Chapter 3 racket workspace

--- chapter3.rkt
+++ chapter3.rkt
@@ -264,5 +264,116 @@
 (define C (make-connector))
 (define F (celsius-fahrenheit-converter-exp C))
 
+; 3.5.1
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+;(define (stream-map proc s)
+;  (if (stream-null? s)
+;      the-empty-stream
+;      (cons-stream (proc (stream-car s))
+;                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (stream-map proc . argstreams)
+   (if (stream-null? (car argstreams))
+       the-empty-stream
+       (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+; 3.5.2
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define (display-stream-to s n)
+  (define (iter s count)
+    (cond ((= count 0) 'done)
+          ((stream-null? s) 'done)
+          (else (display-line (stream-car s))
+                (iter (stream-cdr s) (- count 1)))))
+  (iter s (+ n 1)))
+
+(define (div-streams s1 s2)
+  (stream-map / s1 s2))
+
+(define (integrate-series s) (div-streams s integers))
+
+(define cosine-series
+  (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
+
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+               (add-streams (add-streams (scale-stream (stream-cdr s1) (stream-car s2))
+                                         (scale-stream (stream-cdr s2) (stream-car s1)))
+                            (cons-stream 0 (mul-series (stream-cdr s1) (stream-cdr s2))))))
+
+(define (invert-unit-series s)
+  (if (not (= (stream-car s) 1))
+      (error "Constant term of input stream must be 1 -- INVERT-UNIT-SERIES" (stream-car s))
+      (cons-stream 1
+                   (mul-series (scale-stream (stream-cdr s) -1)
+                               (invert-unit-series s)))))
+
+(define (div-series s1 s2)
+  (if (= (stream-car s2) 0)
+      (error "Denominator series has a zero constant term -- DIV-SERIES" (stream-car s2))
+      (mul-series s1
+                  (scale-stream (invert-unit-series (scale-stream s2
+                                                                  (/ 1 (stream-car s2))))
+                                (stream-car s2)))))
+
+(define tan-series (div-series sine-series cosine-series))
+
 ;;;;;;;;;;;;;; Experimental Below ;;;;;;;;;;;;;;;;;;
 
